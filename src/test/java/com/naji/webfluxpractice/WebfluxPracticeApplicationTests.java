361069f8cb6ed47d46fac5f479bdb3e453ee401c
jihoon.na <jihoon.na@yanolja.com>
2022-02-03 22:27:01 +0900

    Initial commit

---
+++ src/test/java/com/naji/webfluxpractice/WebfluxPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naji.webfluxpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
